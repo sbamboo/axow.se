dbf9ca41c0f45107593af2cf6835940561fc81d3
StalkedBamboo <85576459+sbamboo@users.noreply.github.com>
2024-12-05 22:04:05 +0100

    add wikipage viewer js

--- wiki/minecraft/js/wiki.js
+++ wiki/minecraft/js/wiki.js
@@ -39,8 +39,11 @@ window.onload = () => {
                 if (valid === true) {
                     const markdown = JSON.stringify(pageData);
                     const html = renderMarkdown(markdown);
-                    wikipageContainer.innerHTML = html;
 
+                    wikipageContainer.innerHTML = `<div class="wikipage-wrapper">` + html + `</div> <a href="/wiki/minecraft" class="return-cross"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg></a>`;
+
+                    wikipageContainer.style.display = "block";
+                    wikiContainer.style.display = "none";
                     return;
                 }
             })
